2fa7382e524072cf6d8533f90d289896efe65c9d
erickannuardelcampopatino <243569261+erickannuardelcampopatino@users.noreply.github.com>
2025-11-19 08:18:49 -0600

    slideshow fix

--- templates/Pages/home.php
+++ templates/Pages/home.php
@@ -16,20 +16,20 @@
   <!-- Full-width images with number and caption text -->
   <div class="mySlides fade">
     <div class="numbertext">1 / 3</div>
-    <img src="joyorT7.jpg" style="width:100%">
-    <div class="text">Caption Text</div>
+    <img src="./img/joyorT7.jpg" style="width: 225px">
+    <div class="text">joyorT7</div>
   </div>
 
   <div class="mySlides fade">
     <div class="numbertext">2 / 3</div>
-    <img src="turboantM10.jpg" style="width:100%">
-    <div class="text">Caption Two</div>
+    <img src="./img/turboantM10.jpg" style="width: 225px">
+    <div class="text">turboantM10</div>
   </div>
 
   <div class="mySlides fade">
     <div class="numbertext">3 / 3</div>
-    <img src="xiaomi lite 4.jpg" style="width:100%">
-    <div class="text">Caption Three</div>
+    <img src="./img/xiaomi lite 4.jpg" style="width: 225px">
+    <div class="text">Xiaomi lite 4</div>
   </div>
 
   <!-- Next and previous buttons -->
@@ -51,4 +51,36 @@
         <p>Get to and from with a scooter thats only for you</p>
     </section>
 </body>
+
+<script>
+  let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}  
+</script>
+
 </html>
