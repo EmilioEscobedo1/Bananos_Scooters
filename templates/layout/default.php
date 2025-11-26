2cebc901468ca2559d6aa600a075f7fa89b3d943
Emiliano Hernández <143988389+Exmi88@users.noreply.github.com>
2025-11-26 08:33:05 -0600

    Botones Autenficación Actuzalizados

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -12,29 +12,35 @@
     <?= $this->fetch('script') ?>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 </head>
-<header>
-<img src="/img/logoOnlyScooters.png" alt="logo goes here" id="logo_scooters"><h1>OnlyScooters</h1>
-
-<div class="auth-buttons">
-    <a href="/sesion" class="btn btn-sesion">Iniciar Sesión</a>
-    <a href="/registrarse" class="btn btn-registrarse">Registrarse</a>
-</div>
-
-</header>
-<nav>
-    <ul>
-        <li><a href="/modelos">MODELOS</a></li>
-        <li><a href="/mapa">MAPA</a></li>
-        <li><a href="/promociones">PROMOCIONES</a></li>
-        <li><a href="/rentas">RENTA</a></li>
-        <li><a href="/estaciones">ESTACIONES</a></li>
-    </ul>
-</nav>
 <body>
+    
+    <header>
+        <div class="brand-container">
+            <img src="/img/logoOnlyScooters.png" alt="logo goes here" id="logo_scooters">
+            <h1>OnlyScooters</h1>
+        </div>
+
+        <div class="auth-buttons">
+            <a href="/login">INICIAR SESIÓN</a>
+            <a href="/users/add">REGISTRARSE</a>
+        </div>
+    </header>
+
+    <nav>
+        <ul>
+            <li><a href="/modelos">MODELOS</a></li>
+            <li><a href="/mapa">MAPA</a></li>
+            <li><a href="/promociones">PROMOCIONES</a></li>
+            <li><a href="/rentas">RENTAS</a></li>
+            <li><a href="/estaciones">ESTACIONES</a></li>
+        </ul>
+    </nav>
+
     <main class="main">
         <?= $this->Flash->render() ?>
         <?= $this->fetch('content') ?>
     </main>
+
     <footer>
         <div class="footerContainer">
             <div class="socialIcons">
@@ -43,10 +49,12 @@
                 <a href="whatsapp"><i class="fa-brands fa-whatsapp"></i></a>
             </div>
             <div class="footerLine">
-            <ul>  <li><a href="sobre">NOSOTROS</a></li>
+                <ul>  
+                    <li><a href="sobre">NOSOTROS</a></li>
                     <li><a href="contactanos">CONTACTANOS</a></li>
                     <li><a href="equipo">NUESTRO EQUIPO</a></li>
-            </ul> </div>   
+                </ul> 
+            </div>   
         </div> 
     </footer>
 </body>
